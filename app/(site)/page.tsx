c214e98738b35e1507cda74e10f96cce18c03467
DaronAdams <daron.cole@icloud.com>
2023-05-16 13:37:20 -0500

    added logo for sign in page

---
+++ app/(site)/page.tsx
@@ -0,0 +1,34 @@
+import { AiFillWechat } from 'react-icons/ai'
+
+export default function Home() {
+  return (
+    <div
+    className="
+        flex
+        min-h-full
+        flex-col
+        justify-center
+        py-12
+        sm:px-6
+        lg:px-8
+        "
+    >
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <AiFillWechat className="mx-auto h-12 w-auto" />
+            <h2
+                className='
+                    mt-6
+                    text-center
+                    text-3xl
+                    font-bold
+                    tracking-tight
+                    text-white-100
+                '
+            >
+                Sign in to your account
+            </h2>
+        </div>
+        {/* AuthForm */}
+    </div>
+  )
+}
